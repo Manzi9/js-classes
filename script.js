ead9a05bcee9b700c2421a56ee48a65654ba97d9
Your Name <manzi5000@hotmail.co.uk>
2024-06-10 23:50:55 +0100

    classes attempt

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
+class Garage {
+  constructor(make, model) {
+    this.make = make;
+    this.model = model;
+  }
 
+  getMakeModel() {
+    return `${this.make} ${this.model}`;
+  }
+}
+
+class Car extends Garage {
+  constructor(make, model) {
+    super(make, model);
+  }
+
+  noOfDoors(value) {
+    this.doors = value;
+  }
+}
+
+class Motorbike extends Garage {
+  constructor(make, model) {
+    super(make, model);
+  }
+  mileage(value) {
+    this.mileage = value;
+  }
+}
+
+class engineCC extends Motorbike {
+  constructor(value) {
+    this.engineCC = value;
+  }
+}
+
+const _motorbike = new Motorbike("Honda", "CBR650");
+console.log(_motorbike.getMakeModel);
